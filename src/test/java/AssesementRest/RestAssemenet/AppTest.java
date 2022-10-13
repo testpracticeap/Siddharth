321c88c5d47eb0e90231581f5eb2070a2c0b4125
Prosad <Siddharth.Prosad@sony.com>
2022-10-13 15:52:28 +0530

    get push put delete rest assured

--- src/test/java/AssesementRest/RestAssemenet/AppTest.java
+++ src/test/java/AssesementRest/RestAssemenet/AppTest.java
@@ -20,139 +20,114 @@ import io.restassured.response.ValidatableResponseOptions;
 import io.restassured.specification.RequestSpecification;
 import junit.framework.Assert;
 
-
-
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-	
+
 	@Test
-	    public void getRequest() {
-	        RestAssured.given()
-	        .baseUri("https://dummyjson.com/products")
-	        .contentType(ContentType.JSON).when().get().then().log().all(); }
-	
+	public void getRequest() {
+		RestAssured.given().baseUri("https://dummyjson.com/products").contentType(ContentType.JSON).when().get().then()
+				.log().all();
+	}
+
 	@Test
-    public void GetProductsDetailsHeaderResponse() {
-        
+	public void GetProductsDetailsHeaderResponse() {
 
+		RestAssured.baseURI = "https://dummyjson.com/products";
+		RequestSpecification httpreq = RestAssured.given();
+		Response response = httpreq.request(Method.GET, "");
 
+		response = httpreq.get("");
 
-       RestAssured.baseURI = "https://dummyjson.com/products";
-        RequestSpecification httpreq = RestAssured.given();
-        Response response = httpreq.request(Method.GET, "");
-        
-         response = httpreq.get("");
-         
-         Headers header=response.headers();
-         
-         
-         for(Header headersec:header)
-         {
-                         System.out.println("value "+headersec.getName() +" "+":"+headersec.getValue() );
-         }
-    }
-@Test
-public void GetProductsDetailsHeaderResponseoneAtaTime() {
-    
+		Headers header = response.headers();
 
+		for (Header headersec : header) {
+			System.out.println("value " + headersec.getName() + " " + ":" + headersec.getValue());
+		}
+	}
 
+	@Test
+	public void GetProductsDetailsHeaderResponseoneAtaTime() {
 
-   RestAssured.baseURI = "https://dummyjson.com/products";
-    RequestSpecification httpreq = RestAssured.given();
-    Response response = httpreq.request(Method.GET, "");
-    
-     response = httpreq.get("");
-     
-     String header=response.header("Server");
-     System.out.println(header );
-     String header1=response.header("Content-Type");
-     System.out.println(header1 );
-    
-    
-    
+		RestAssured.baseURI = "https://dummyjson.com/products";
+		RequestSpecification httpreq = RestAssured.given();
+		Response response = httpreq.request(Method.GET, "");
 
+		response = httpreq.get("");
 
+		String header = response.header("Server");
+		System.out.println(header);
+		String header1 = response.header("Content-Type");
+		System.out.println(header1);
 
-}
-@Test
-public void GetProductsDetailsResponseBody() {
-    
+	}
 
+	@Test
+	public void GetProductsDetailsResponseBody() {
 
+		RestAssured.baseURI = "https://dummyjson.com/products";
+		RequestSpecification httpreq = RestAssured.given();
+		Response response = httpreq.request(Method.GET, "");
 
-   RestAssured.baseURI = "https://dummyjson.com/products";
-    RequestSpecification httpreq = RestAssured.given();
-    Response response = httpreq.request(Method.GET, "");
-    
-     response = httpreq.get("");
-     
-     ResponseBody resbody=response.getBody();
-     
-     System.out.println(resbody.asString());
-     
-    
-    
+		response = httpreq.get("");
 
+		ResponseBody resbody = response.getBody();
 
+		System.out.println(resbody.asString());
 
-}
-	
-	
-	
-	
-	
+	}
+
 //post
- @Test
-public void postRequestUsingStaticFile() {
-    File jsonData= new File ("C:\\Users\\7000032450\\eclipse-workspace\\RestAssemenet\\src\\resources\\java\\json\\widget.json");
-    
-    RestAssured.given()
-    .baseUri("https://dummyjson.com/products")
-    .contentType(ContentType.JSON).body(jsonData)
-     .when().post("/add").then().log().all().assertThat().statusCode(200);
+	@Test
+	public void postRequestUsingStaticFile() {
+		File jsonData = new File(
+				"C:\\Users\\7000032450\\eclipse-workspace\\RestAssemenet\\src\\resources\\java\\json\\widget.json");
 
-}
+		RestAssured.given().baseUri("https://dummyjson.com/products").contentType(ContentType.JSON).body(jsonData)
+				.when().post("/add").then().log().all().assertThat().statusCode(200);
 
-	 //put request
-   @Test
-    public void putRequest() throws IOException {
-          
-        byte[] b = Files.readAllBytes(Paths.get("src/resources/java/json/widget.json"));
-         //convert byte array to string
-          String bdy = new String(b);
-          
-         //base URL
-          RestAssured.given()
-          
-            .baseUri("https://dummyjson.com").contentType(ContentType.JSON).body(bdy)
+	}
 
-         //adding post method
-          .when().put("/products/1").then().log().all()
+	// put request
+	@Test
+	public void putRequest() throws IOException {
 
-         //verify status code as 201
-          .assertThat().statusCode(200);
-        
-    }
-    //Delete Request
-    @Test
-    public void deleteRequest() throws IOException {
-         
-        byte[] b = Files.readAllBytes(Paths.get("src/resources/java/json/widget.json"));
+		byte[] b = Files.readAllBytes(Paths.get("src/resources/java/json/widget.json"));
+		// convert byte array to string
+		String bdy = new String(b);
 
-         //convert byte array to string
-          String bdy = new String(b);
+		// base URL
+		RestAssured.given()
 
-         //base URL
-          RestAssured.given()
-          
-            .baseUri("https://dummyjson.com").contentType(ContentType.JSON).body(bdy)
+				.baseUri("https://dummyjson.com").contentType(ContentType.JSON).body(bdy)
 
-         //adding delete method
-          .when().delete("/products/3").then().log().all()
+				// adding post method
+				.when().put("/products/1").then().log().all()
 
-         //verify status code as 201
-          .assertThat().statusCode(200);
-}
+				// verify status code as 201
+				.assertThat().statusCode(200);
+
+	}
+
+	// Delete Request
+	@Test
+	public void deleteRequest() throws IOException {
+
+		byte[] b = Files.readAllBytes(Paths.get("src/resources/java/json/widget.json"));
+
+		// convert byte array to string
+		String bdy = new String(b);
+
+		// base URL
+		RestAssured.given()
+
+				.baseUri("https://dummyjson.com").contentType(ContentType.JSON).body(bdy)
+
+				// adding delete method
+				.when().delete("/products/3").then().log().all()
+
+				// verify status code as 201
+				.assertThat().statusCode(200);
+	}
 }
