b1ae9e346c3498a33c0165b27de8100398cdec05
Prosad <Siddharth.Prosad@sony.com>
2022-10-14 17:13:49 +0530

    code of assessement

--- src/test/java/Assesement/Assesement/Post.java
+++ src/test/java/Assesement/Assesement/Post.java
@@ -12,24 +12,21 @@ import io.restassured.response.Response;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
-
-
-/**
+/*
  * Unit test for simple App.
  */
 public class Post {
-	
+
 	@Test
 	public void postmethod() throws JsonProcessingException {
-	File jsonData=new File("C:/Users/7000032450/eclipse-workspace/Assesement/json/json1/data1.json");
-	 RestAssured.given().header("Authorization","Bearer 24f4362e9a1a17afa8ad5da989bae75c6fe09f271913f5a71c27b88726157fed").
-	baseUri("https://gorest.co.in/public/v1/users").
-	contentType(ContentType.JSON).body(jsonData).when().post().then().
-	log().all();
-	//ObjectMapper=new ObjectMapper()
-		//	ObjectMapper MAPPER = new ObjectMapper();
-	//String json=MAPPER.writeValueAsString(jsonData);
+		File jsonData = new File("C:/Users/7000032450/eclipse-workspace/Assesement/json/json1/data1.json");
+		RestAssured.given()
+				.header("Authorization", "Bearer 24f4362e9a1a17afa8ad5da989bae75c6fe09f271913f5a71c27b88726157fed")
+				.baseUri("https://gorest.co.in/public/v1/users").contentType(ContentType.JSON).body(jsonData).when()
+				.post().then().log().all();
+		// ObjectMapper=new ObjectMapper()
+		// ObjectMapper MAPPER = new ObjectMapper();
+		// String json=MAPPER.writeValueAsString(jsonData);
 //	Response response = RestAssured.given()
 //            .header("Authorization","Bearer 24f4362e9a1a17afa8ad5da989bae75c6fe09f271913f5a71c27b88726157fed").
 //            contentType("application/json").
@@ -42,28 +39,17 @@ public class Post {
 //	JsonPath jpath=new JsonPath(postresponse);
 //	jpath.get("e");
 //	String str=jpath.getString("email");
-Response response2 =RestAssured.given().
-header("Authorization","Bearer 24f4362e9a1a17afa8ad5da989bae75c6fe09f271913f5a71c27b88726157fed").
-baseUri("https://gorest.co.in/public/v1/users").
-queryParam("email","siddharth14@matrix.com").when().get();
-System.out.println(response2.asString());
+		Response response2 = RestAssured.given()
+				.header("Authorization", "Bearer 24f4362e9a1a17afa8ad5da989bae75c6fe09f271913f5a71c27b88726157fed")
+				.baseUri("https://gorest.co.in/public/v1/users").queryParam("email", "siddharth14@matrix.com").when()
+				.get();
+		System.out.println(response2.asString());
 
-io.restassured.path.json.JsonPath jsonpath= response2.jsonPath();
+		io.restassured.path.json.JsonPath jsonpath = response2.jsonPath();
 
+		String ID = jsonpath.getString("data[0].id");
 
-String ID = jsonpath.getString("data[0].id");
+		System.out.println(ID);
+	}
 
-System.out.println(ID);
 }
-
-}	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
